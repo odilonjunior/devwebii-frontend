fce8bbe8ecc2ccb74ba8ffef11d5e4469984e5d8
odilonjunior <37005766+odilonjunior@users.noreply.github.com>
2021-07-07 00:43:59 -0300

    Estrutura feita

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
+import React from 'react';
+import SignIn from './components/Signin';
+import Register from './components/Register';
+import Checkout from './components/Checkout';
+import Dashboard from './components/Dashboard';
+import PageNotFound from './components/PageNotFound';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
 
 const App = () => {
-  return (
-    <h1>Aplicação</h1>
-  );
+  return(
+    <BrowserRouter>
+    <Switch>
+      <Route path='/' exact={true} component={SignIn}/>
+      <Route path='/register' component={Register}/>
+      <Route path='/checkout' component={Checkout}/>
+      <Route path='/dashboard' component={Dashboard}/>
+      <Route path='*' component={PageNotFound}/>
+      
+    </Switch>
+  </BrowserRouter>
+
+  )
+  
 }
 
 export default App;
---
+++ frontend/src/components/PageNotFound/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Copyright from '../Common/Copyright';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
+
+export default function PageNotFound() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          404!
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom>
+          {'A página procurada não foi localizada. '}
+          {'Verifique o endereço digitado.'}
+        </Typography>
+        <Typography variant="body1">404! - Page Not Found</Typography>
+      </Container>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">My sticky footer can be found here.</Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
